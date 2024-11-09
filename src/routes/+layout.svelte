4c9f0c8317371f698e20e608bbec78225b6ce961
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-11-09 23:33:15 +0000

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,4 @@
   let { children } = $props();
 </script>
 
-<svelte:head>
-  <title>Masjid Al-Tawheed</title>
-  <meta name="description" content="The expansion project for Masjid Al-Tawheed in Elk Grove." />
-  <meta
-    name="keywords"
-    content="Masjid Al-Tawheed, New Masjid Project, Masjid Expansion, Tawheed Elk Grove" />
-</svelte:head>
-
 {@render children()}
