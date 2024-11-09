84e6c92740dbabc572535d29e98f3bae1a47a8f3
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-11-09 23:17:09 +0000

    Navigation bar

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  let { width = 1200, bgColorClass = null, children }: { width: number; bgColorClass: string | null, children: Snippet } =
+    $props();
+</script>
+
+<div class="h-full {bgColorClass}">
+  <div class="mx-auto h-full" style="max-width: {width}px">
+    {@render children()}
+  </div>
+</div>
---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import FaFacebookLogo from 'svelte-icons/fa/FaFacebookSquare.svelte';
+  import FaInstagramLogo from 'svelte-icons/fa/FaInstagram.svelte';
+  import Container from './Container.svelte';
+</script>
+
+<Container bgColorClass="bg-[#131313]">
+  <div class="flex flex-col items-center justify-center px-8 py-6">
+    <div class="flex w-full flex-col">
+      <div class="flex w-full flex-wrap justify-between gap-5">
+        <a href="/" aria-label="Home Page">
+          <img
+            src="/images/tawheed.svg"
+            alt="Masjid Al-Tawheed logo"
+            class="aspect-square w-[48px] object-contain" />
+        </a>
+
+        <div class="flex gap-8">
+          <a
+            href="https://www.instagram.com/masjidtawheedeg/"
+            aria-label="Tawheed Instagram Account"
+            class="h-[48px] w-[48px] text-white">
+            <FaInstagramLogo />
+          </a>
+          <a
+            href="https://www.facebook.com/EG.Tawheed/"
+            aria-label="Tawheed Facebook Account"
+            class="h-[48px] w-[48px] text-white">
+            <FaFacebookLogo />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</Container>
