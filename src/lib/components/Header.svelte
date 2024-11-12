e0e7b04ee2ac4c971674cf8dad4ffd3e253022ce
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-11-12 09:29:11 +0000

    design changes, still wip for header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,26 +4,29 @@
   import FaFacebookLogo from 'svelte-icons/fa/FaFacebookSquare.svelte';
 </script>
 
-<Container bgColorClass="bg-[#131313]">
+<Container bgColorClass="bg-gradient-to-r from-[#131313] via-[#161616] via-[#1b1b1b] to-[#252525]">
   <div class="flex flex-col items-center justify-center py-5">
     <div class="flex w-full flex-col">
       <div class="flex w-full flex-wrap justify-between">
         <a href="/" aria-label="Website home page">
-          <img src="images/tawheed.svg" alt="Tawheed Logo" class="h-[48px] w-[48px]" />
+          <img
+            src="images/tawheed.svg"
+            alt="Tawheed Logo"
+            class="h-[48px] w-[48px] hover:brightness-125" />
         </a>
 
         <div class="flex gap-8">
           <a
             href="https://www.instagram.com/masjidtawheedeg/"
             aria-label="Masjid Al-Tawheed Instagram account"
-            class="h-[48px] w-[48px] text-white">
+            class="h-[48px] w-[48px] text-gray-100 hover:text-white">
             <FaInstagramLogo />
           </a>
 
           <a
             href="https://www.facebook.com/EG.Tawheed/"
             aria-label="Masjid Al-Tawheed Facebook account"
-            class="h-[48px] w-[48px] text-white">
+            class="h-[48px] w-[48px] text-gray-100 hover:text-white">
             <FaFacebookLogo />
           </a>
         </div>
@@ -41,9 +44,19 @@
             <p class="text-2xl font-normal text-gray-100">
               Help us in funding the expansion project for Masjid Al-Tawheed in Elk Grove.
             </p>
+
+            <div class="mt-4 flex flex-col gap-8 md:flex-row lg:flex-row">
+              <a href="/" aria-label="Donate now" class="text-lg font-semibold text-gray-100 shadow-md border p-2 rounded-lg bg-[#24418C]">
+                Donate Now
+              </a>
+
+              <a href="/" aria-label="Learn More" class="text-lg font-semibold text-gray-100 shadow-md border p-2 rounded-lg bg-[#22A34A]">
+                Learn More
+              </a>
+            </div>
           </div>
 
-          <div class="mt-5 flex w-full justify-center md:mt-0 md:w-1/2 lg:w-1/2">
+          <div class="mt-5 flex w-full justify-center md:mt-0 md:w-2/3 lg:w-3/4">
             <img
               src="/images/masjid1.webp"
               alt="Proposed masjid expansion"
