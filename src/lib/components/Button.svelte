4ab2320d2273620050a0677944e104acbd1f390b
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-11-20 07:03:39 +0000

    separate button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let {
+    color = 'jade',
+    href,
+    text
+  }: { color: 'jade' | 'azure'; href: string; text: string } = $props();
+</script>
+
+<a
+  class="text-center {color === 'jade'
+    ? 'border-jade bg-jade hover:bg-jade-dark'
+    : 'border-azure bg-azure hover:bg-azure-dark'} rounded-lg border-2 px-4 py-2 font-semibold tracking-wide text-gray-100 shadow-md transition hover:-translate-y-1"
+  {href}>{text}</a>
