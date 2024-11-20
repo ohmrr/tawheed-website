4e3783b7cee9d653e3cac0101fad6aa6a784213b
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-11-20 07:04:09 +0000

    fix main page layout on larger screens

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,50 +1,61 @@
 <script lang="ts">
+  import MasjidExpansion from '$lib/assets/tawheed/masjid1.webp';
+  import Button from '$lib/components/Button.svelte';
   import Container from '$lib/components/Container.svelte';
   import ProgressBar from '$lib/components/ProgressBar.svelte';
-  import TawheedLogo from '$lib/assets/tawheed/tawheed.png';
 </script>
 
 <Container bgColorClass="bg-white">
-  <div class="flex flex-col items-center justify-center py-12 md:py-16">
-    <div class="flex w-full flex-col items-center justify-center">
-      <h2 class="mb-8 text-center text-3xl font-semibold text-gray-900 md:text-4xl lg:text-5xl">
-        Fund Masjid Al-Tawheed
-      </h2>
+  <div class="mt-16 md:mt-24 lg:mt-32 flex w-full flex-col items-center justify-center">
+    <h2 class="text-center text-3xl font-semibold text-gray-900 md:text-4xl lg:text-5xl">
+      Help Create a Thriving Muslim Community
+    </h2>
 
-      <div class="mb-8 flex w-full justify-center sm:w-2/3 lg:w-3/5">
-        <img
-          src={TawheedLogo}
-          alt="Masjid Al-Tawheed logo"
-          class="h-full w-full max-w-lg rounded-lg object-cover" />
+    <div class="grid grid-cols-1 gap-y-8 pt-8 md:grid-cols-2 md:gap-x-4 md:gap-y-0 lg:gap-x-8">
+      <img
+        class="rounded-lg object-contain shadow-md"
+        src={MasjidExpansion}
+        alt="Proposed masjid expansion" />
+
+      <div
+        class="flex flex-col justify-start rounded-lg bg-gray-200 pt-4 shadow-md md:justify-center">
+        <h3 class="text-center font-arabic text-xl font-semibold text-gray-900 md:text-2xl">
+          ‏مَنْ بَنَى مَسْجِدًا لِلَّهِ كَمَفْحَصِ قَطَاةٍ أَوْ أَصْغَرَ بَنَى اللَّهُ لَهُ بَيْتًا
+          فِي الْجَنَّةِ
+        </h3>
+
+        <blockquote class="pt-4 text-center italic text-gray-800 md:text-lg">
+          “The one who builds a Masjid for the pleasure of Allah Almighty, Allah Almighty will build
+          a house for him in paradise.” <br />
+          <span class="font-semibold">- Sahih Muslim</span>
+        </blockquote>
       </div>
+    </div>
 
-      <div class="flex w-full flex-col items-center justify-center gap-y-8">
-        <div class="rounded-lg bg-gray-100 p-6 shadow-md">
-          <h3 class="mb-6 text-center font-arabic text-2xl font-semibold text-gray-900 md:text-3xl">
-            ‏مَنْ بَنَى مَسْجِدًا لِلَّهِ كَمَفْحَصِ قَطَاةٍ أَوْ أَصْغَرَ بَنَى اللَّهُ لَهُ
-            بَيْتًا فِي الْجَنَّةِ
-          </h3>
+    <div class="mt-8 md:mt-16 flex w-full flex-col items-center gap-y-8 md:w-3/4">
+      <p class="text-center text-base text-gray-900 tracking-wide md:text-xl">
+        The growth of our community has led to a need for a space that can accommodate all of our
+        needs, be it spiritual, educational, communal, or even recreational. By investing in this
+        expansion, you're investing in a legacy of faith and kindness. Every prayer, smile, and
+        shared meal in this expanded space will be a blessing. No amount is too small, every dollar
+        brings us one step closer to creating this sacred place for our community.
+      </p>
 
-          <p class="text-center text-base font-normal italic text-gray-800 md:text-lg">
-            “The one who builds a Masjid for the pleasure of Allah Almighty, Allah Almighty will
-            build a house for him in paradise.” <br />
-            <span class="font-semibold">- Sahih Muslim</span>
-          </p>
-        </div>
+      <p class="text-center text-base tracking-wide font-semibold text-gray-900 md:text-xl">
+        Let's unite to make Masjid Al-Tawheed a home for all. Together, we can provide a welcoming
+        sanctuary that will support our community's needs and inspire growth, connection, and
+        compassion. Your donation today can transform lives and build a stronger future.
+      </p>
+    </div>
 
-        <p class="text-center text-gray-900 md:text-left md:text-lg">
-          The growth of our community has led to a need for a space that can accommodate all of our
-          needs, be it spiritual, educational, communal, or even recreational. By investing in this
-          expansion, you're investing in a legacy of faith and kindness. Every prayer, smile, and
-          shared meal in this expanded space will be a blessing. No amount is too small, every
-          dollar brings us one step closer to creating this sacred place for our community.
-        </p>
-
-        <p class="text-center font-semibold text-gray-900 md:text-left md:text-lg">
-          Let's unite to make Masjid Al-Tawheed a home for all. Together, we can provide a welcoming
-          sanctuary that will support our community's needs and inspire growth, connection, and
-          compassion. Your donation today can transform lives and build a stronger future.
-        </p>
+    <div class="mt-12 md:mt-16 flex w-full flex-col items-center justify-center">
+      <div
+        class="flex flex-row items-center mb-12 justify-center gap-x-4 lg:gap-x-8">
+        <Button
+          color="jade"
+          href="https://checkout.square.site/merchant/MLJGAABQWSY7N/checkout/LMCROJTOGFCEPQJM5VAPH7VX"
+          text="Donate Now" />
+        <Button color="azure" href="https://www.masjidaltawheed.com/newmasjid/" text="Learn More" />
       </div>
 
       <ProgressBar />
