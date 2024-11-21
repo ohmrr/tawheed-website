07577e115270aeb069d896a7976281c6f8b59d1c
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-11-21 22:57:32 +0000

    finally fix, use object.values instead of object.keys

--- src/routes/(main)/+page.ts
+++ src/routes/(main)/+page.ts
@@ -1,15 +1,18 @@
 import type { PageLoad } from './$types';
 
-const imageModules = import.meta.glob('$lib/assets/community/**/*.{jpg,jpeg,png,webp}', { query: '?url', import: 'default', eager: true });
-const images = Object.keys(imageModules);
+const imageModules: Record<string, string> = import.meta.glob('$lib/assets/community/**/*.{jpg,jpeg,png,webp}', {
+  query: '?url',
+  import: 'default',
+  eager: true
+});
 
-const alts = images.map(path => {
+const images = Object.values(imageModules);
+
+const alts = Object.keys(imageModules).map((path) => {
   const fileName = path.split('/').pop() || 'Image';
-
   return fileName.slice(0, fileName.lastIndexOf('.')).replace(/[-_]/g, ' ');
 });
 
 export const load = (() => {
   return { images, descriptions: alts };
 }) satisfies PageLoad;
-
