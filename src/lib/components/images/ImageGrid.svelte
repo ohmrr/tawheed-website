6a0fa44993a30cb38dc881e236b78e9466f8d874
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-11-21 00:17:55 +0000

    rename images to have more descriptive alts

--- src/lib/components/images/ImageGrid.svelte
+++ src/lib/components/images/ImageGrid.svelte
@@ -8,22 +8,22 @@
   }
 
   let images: Image[] = [];
-  let showModal = false;
-  let currentImage: Image | null = null;
+  // let showModal = false;
+  // let currentImage: Image | null = null;
 
   onMount(() => {
     images = importImages();
   });
 
-  function openModal(image: Image) {
-    currentImage = image;
-    showModal = true;
-  }
+  // function openModal(image: Image) {
+  //   currentImage = image;
+  //   showModal = true;
+  // }
 
-  function closeModal() {
-    showModal = false;
-    currentImage = null;
-  }
+  // function closeModal() {
+  //   showModal = false;
+  //   currentImage = null;
+  // }
 </script>
 
 <div class="grid grid-cols-2 gap-2 md:grid-cols-3">
@@ -32,28 +32,7 @@
       <img
         src={image.src}
         alt={image.alt}
-        class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
-        on:click={() => openModal(image)} />
+        class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105" />
     </div>
   {/each}
 </div>
-
-{#if showModal}
-  <div
-    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70"
-    on:click={closeModal}>
-    <div
-      class="relative w-11/12 max-w-4xl overflow-hidden rounded-lg bg-white shadow-lg"
-      on:click|stopPropagation>
-      <img
-        src={currentImage?.src}
-        alt={currentImage?.alt}
-        class="h-auto max-h-[80vh] w-full object-contain" />
-      <button
-        class="absolute right-4 bg-gray-50 py-0.5 px-1 shadow-md rounded-lg top-4 text-gray-800 hover:text-gray-900 font-bold text-xl"
-        on:click={closeModal}>
-        &#10005;
-      </button>
-    </div>
-  </div>
-{/if}
