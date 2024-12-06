a78c0150ba8ad18ff084e7b53f90d23dbc3808b5
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-12-06 09:03:46 +0000

    change tawheed logo hover style

--- src/lib/components/navbar/TawheedLogo.svelte
+++ src/lib/components/navbar/TawheedLogo.svelte
@@ -6,5 +6,5 @@
   <img
     src={TawheedSVG}
     alt="Tawheed Logo"
-    class="h-12 w-12 transition duration-300 ease-in-out hover:brightness-125" />
+    class="h-12 w-12 transition duration-300 ease-in-out hover:brightness-75" />
 </a>
