d78a8c4bd155de5127de198f952a0a5f432d9dfa
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-12-13 09:52:54 +0000

    make the last photo of the grid fill both columns on mobile if the images length is odd

--- src/lib/components/images/ImageGrid.svelte
+++ src/lib/components/images/ImageGrid.svelte
@@ -8,11 +8,11 @@
 </script>
 
 <div class="mt-6 grid grid-cols-2 gap-1.5 md:grid-cols-3">
-  {#each images as image}
-    <div class="group relative cursor-pointer overflow-hidden rounded-md shadow-lg">
+  {#each images as { src, alt }, index}
+    <div class="group relative cursor-pointer overflow-hidden rounded-md shadow-lg { index === images.length - 1 && images.length % 2 !== 0 ? "col-span-2 md:col-span-1" : "" }">
       <enhanced:img
-        src={image.src}
-        alt={image.alt}
+        {src}
+        {alt}
         loading="lazy"
         class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105" />
     </div>
