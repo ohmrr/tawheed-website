0edbd20ee3f9d5c7724bf62d4901ffba1b87c6ca
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-12-17 22:27:32 +0000

    fix webmanifest source, add open graph for sharing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+  import MasjidRender from '$lib/assets/tawheed/masjid2.jpg';
   import '../app.css';
 
   let { children } = $props();
 </script>
+
+<svelte:head>
+  <meta property="og:image" content={MasjidRender} />
+  <meta property="twitter:image" content={MasjidRender} />
+</svelte:head>
 
 {@render children()}
